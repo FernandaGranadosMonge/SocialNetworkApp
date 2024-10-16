ef07ce1f741f4f4a494865dadf498ad5082ff7c5
FernandaGranadosMonge <A01252895@tec.mx>
2024-10-15 18:23:58 -0700

    Add: Login page view and functionalities. MISSING: Connecting to real screens instead of temporary screens 'Feed' and 'Sign Up'

--- SocialNetwork/App.js
+++ SocialNetwork/App.js
@@ -3,6 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LogInScreen from './Screens/LogInScreen';
+import SignUpScreen from './Screens/SignUpScreen';
+import FeedScreen from './Screens/Feed';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +13,8 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={styles.screenTitle}>
         <Stack.Screen name="Log In" component={LogInScreen} />
+        <Stack.Screen name="Sign Up" component={SignUpScreen} />
+        <Stack.Screen name="Feed" component={FeedScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
